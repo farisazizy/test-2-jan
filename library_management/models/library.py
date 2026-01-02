fe23b8a337e6c677d7c161523a0cc137a9d8e55f
Faris Azizy <farisazizy@dattora.co>
2026-01-02 14:39:41 +0700

    Add Category

--- library_management/models/library.py
+++ library_management/models/library.py
@@ -5,9 +5,11 @@ class LibraryBook(models.Model):
     _name = 'library.book'
     _description = 'Library Book'
 
-    title = fields.Char(string='Title')
+    title = fields.Char(string='Title', required=True)
     author_id = fields.Many2one('res.partner', string='Author')
     price = fields.Float(string='Price')
+    
+    category_id = fields.Many2one('library.category', string='Categories', required=True)
 
     @api.constrains('price')
     def _check_price_below_zero(self):
@@ -15,4 +17,10 @@ class LibraryBook(models.Model):
             if record.price < 0:
                 raise ValidationError("Price must be more than zero.")
             
-            
+class LibraryCategory(models.Model):
+    _name = 'library.category'
+    _description = 'Library Category'
+
+    category_name = fields.Char(string='Category Name')
+    description = fields.Text(string='Description')
+
