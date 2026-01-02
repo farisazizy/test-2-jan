4400bb112edd39d87f2e75df971ddae4964df3fd
Faris Azizy <farisazizy@dattora.co>
2026-01-02 14:17:40 +0700

    Initial Set-up

---
+++ library_management/__manifest__.py
@@ -0,0 +1,10 @@
+{
+    'name': 'Library Management',
+    'version': '1.0',
+    'depends': ['base'],
+    'data': [
+        'views/library.xml',
+    ],
+    'installable': True,
+    'application': False,
+}
